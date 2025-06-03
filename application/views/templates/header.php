e7577b9a23a003656d2e1308cd1eb772a7b62e5d
Hanas <hanasoke@gmail.com>
2025-06-03 14:59:31 +0700

    updated users View

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,9 +7,3 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   </head>
   <body>
-    <div class="container mt-4">
-      <?php if ($this->session->flashdata('success')): ?>
-        <div class="alert alert-success">
-          <?php echo $this->session->flashdata('success'); ?>
-        </div>
-      <?php endif; ?>
--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,30 +1,100 @@
-<h1>Users List</h1>
-<a href="<?php echo site_url('users/create'); ?>" class="btn btn-primary mb-3" >Add New User</a>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-<table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach($users as $user): ?>
-            <tr>
-                <td><?= $user['user_id']; ?></td>
-                <td><?= $user['name']; ?></td>
-                <td><?= $user['username']; ?></td>
-                <td><?= $user['email']; ?></td>
-                <td>
-                    <a href="<?php echo site_url('users/edit/'.$user['user_id']); ?>" class="btn btn-warning btn-sm">Edit</a>
-                    
-                    <a href="<?php echo site_url('users/delete/' .$user['user_id']);  ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?') " >Delete</a>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="container mt-4">
+
+    <div class="row mb-3">
+        <div class="col-6 col-md-9 col-lg-10">
+            <h3>Users List</h3>
+        </div>
+        <div class="col-6 col-md-3 col-lg-2">
+            <p class="float-end"><a href="<?php echo site_url('users/create'); ?>" class="btn btn-primary">Add New User</a></p>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col">
+            <?php if ($this->session->flashdata('success')): ?>
+                <div class="alert alert-success">
+                    <?php echo $this->session->flashdata('success'); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($users as $user): ?>
+                        <tr>
+                            <td><?= $user['user_id']; ?></td>
+                            <td><?= $user['name']; ?></td>
+                            <td><?= $user['username']; ?></td>
+                            <td><?= $user['email']; ?></td>
+                            <td><?= $user['address']; ?></td>
+                            <td>
+                                <a href="<?php echo site_url('users/edit/'.$user['user_id']); ?>" class="btn btn-warning btn-sm">Edit</a>
+                                
+                                <a href="<?php echo site_url('users/delete/' .$user['user_id']);  ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?') " >Delete</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    
+
+</div>
+
+
+
+
