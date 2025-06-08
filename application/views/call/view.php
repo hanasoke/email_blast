298a260a175a51b3e940327e7b76ff024ac9b111
Hanas <hanasoke@gmail.com>
2025-06-08 21:53:41 +0700

    update view.php om call folder

--- application/views/call/view.php
+++ application/views/call/view.php
@@ -1,3 +1,22 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="<?php echo site_url('users'); ?>">Email Blast</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link"  href="<?php echo site_url('users'); ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo site_url('call'); ?>">Call</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 <!-- Add this to your existing view file -->
 <div class="container mt-4">
     <?php if($this->session->flashdata('success')): ?>
